687359982b2aba954d093d1af1fae6128ff456c9
Jonathan Leaver <jonathan@jet.com>
2016-11-17 13:46:10 -0500

    alternate listview row backgrounds

---
+++ ClearAzureQueues/Converters/ListViewItemColorConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ClearAzureQueues.Converters {
+
+    [ValueConversion(typeof(ListViewItem), typeof(Brush))]
+    public class ListViewItemColorConverter : IValueConverter {
+
+        public Brush Even { get; set; }
+        public Brush Odd { get; set; }
+
+        public ListViewItemColorConverter() {
+            Even = SystemColors.WindowBrush;
+            Odd = SystemColors.WindowBrush;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            var item = value as ListViewItem;
+            if (item == null) {
+                return SystemColors.WindowBrush;
+            } else {
+                var container = ItemsControl.ItemsControlFromItemContainer(item) as ListView;
+                var index = container.ItemContainerGenerator.IndexFromContainer(item);
+                return index % 2 == 0 ? Even : Odd;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
